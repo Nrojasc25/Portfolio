2391c599b7565465b77506b8ba2827d001b8bc17
nrojas <natalia.rojas@ufl.edu>
2023-11-06 23:51:15 -0600

    Commit with my updated information

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'aboutme': '#A2E4FF', //72A9bE A2E4FF
+      'experience': '#DF6F3F', //FD652F
+      'leadership': '#3A8660', //0070C0 001f5b 
+      'projects': '#001f5b', //001f5b 
+      'contactus': '#D33A02', //D33A02
+      'white': '#ffffff', 
+      'textColor': '#001F5B' 
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
